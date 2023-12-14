630c630d60c65ee217504a6997d002fb45b86a97
damkols <damolausmankolapo@gmail.com>
2023-12-14 16:38:02 +0100

    creating and using a structured logger

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,53 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
-	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request){
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w,r)
 		return
 	}
 
-	files := []string{
+	files:= []string {
 		"./ui/html/base.tmpl.html",
 		"./ui/html/pages/home.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
 	}
 
-	ts, err := template.ParseFiles(files...)
+	ts, err:= template.ParseFiles(files...)
+
 	if err != nil {
-		log.Print(err.Error())
+		app.logger.Error(err.Error(), "method", r.Method, "url", r.URL.RequestURI())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
+
 	if err != nil {
-		log.Print(err.Error())
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.RequestURI)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 0 {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request){
+	id, err :=strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
 		http.NotFound(w,r)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d .....", id)
+
+	log.Printf("Displaying snippet with id %d", id)
+		w.Write([]byte("Displaying Snippets here ...."))
+
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", http.MethodPost)
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create snippets here...."))
+
+	w.Write([]byte("Creating Snippet here ...."))
 }
+
+
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+
+
+	addr := flag.String("addr", "localhost:4000", "HTTP Address Network")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
 
-	fileserver := http.FileServer(http.Dir("./ui/static"))
+	fileserver := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	log.Print("starting server at localhost:4000")
+	logger.Info("starting server at", "addr", *addr)
 
-	err := http.ListenAndServe("localhost:4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
